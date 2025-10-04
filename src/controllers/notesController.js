5b4609ab7fd0ae6b54766302196b4c2b512011a5
volochayev <stingray8888831@gmail.com>
2025-10-04 20:39:15 +0300

    crct mstk wth getNotesById

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -11,6 +11,7 @@ export const getNoteById = async (req, res, next) => {
   const note = await Note.findById(noteId);
   if (!note) {
     next(createHttpError(404, 'Note not found'));
+    return;
   }
   res.status(200).json(note);
 };

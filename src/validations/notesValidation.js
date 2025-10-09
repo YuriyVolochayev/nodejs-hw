a955a065134bc344482e4b03b93693503f14c968
volochayev <stingray8888831@gmail.com>
2025-10-10 00:34:08 +0300

    crkt mstk

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -22,13 +22,15 @@ export const noteIdSchema = {
 };
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).required().messages({
+    title: Joi.string().min(1).max(150).required().messages({
       'string.base': 'Title must be a string',
       'string.min': 'Title should have at least {#limit} characters',
+      'string.max': 'Title should have at most {#limit} characters',
       'any.required': 'Title is required',
     }),
-    content: Joi.string().allow('').messages({
-      'content.base': 'Title must be a string',
+    content: Joi.string().max(900).allow('').messages({
+      'string.base': 'Content must be a string',
+      'string.max': 'Content should have at most {#limit} characters',
     }),
     tag: Joi.string()
       .valid(...TAGS)

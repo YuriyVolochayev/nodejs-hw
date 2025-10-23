a5feb2b20f4e65dd36310e9dc78c2da0b0ce2340
volochayev <stingray8888831@gmail.com>
2025-10-24 00:21:17 +0300

    crct mstk

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,7 @@ import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 import handlebars from 'handlebars';
 import path from 'node:path';
 import fs from 'node:fs/promises';
